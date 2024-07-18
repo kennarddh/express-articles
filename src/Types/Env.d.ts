fb4592930c4025caa5df867fc6709a97b6612dd4
kennarddh <79390992+kennarddh@users.noreply.github.com>
2024-07-18 17:51:09 +0700

    chore: Setup db

--- src/Types/Env.d.ts
+++ src/Types/Env.d.ts
@@ -7,8 +7,6 @@ declare global {
 			REFRESH_JWT_SECRET: string
 			JWT_EXPIRE: string
 			REFRESH_JWT_EXPIRE: string
-			RATE_LIMITER_MAX: string
-			RATE_LIMITER_WINDOW_MS: string
 			LOG_LEVEL: string
 			NODE_ENV: string
 		}
