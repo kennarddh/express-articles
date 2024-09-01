5e7236196ab2c4b2f0f170a0ff062737ab20949e
kennarddh <79390992+kennarddh@users.noreply.github.com>
2024-09-01 09:50:12 +0700

    feat: Init minio buckets

--- src/Database/Minio.ts
+++ src/Database/Minio.ts
@@ -1,5 +1,11 @@
 import * as Minio from 'minio'
 
+import Logger from 'Utils/Logger/Logger'
+
+export enum MinioBucket {
+	ArticlesImages = 'ArticlesImages',
+}
+
 const MinioClient = new Minio.Client({
 	endPoint: process.env.MINIO_ENDPOINT,
 	port: parseInt(process.env.MINIO_PORT, 10),
@@ -7,5 +13,17 @@ const MinioClient = new Minio.Client({
 	accessKey: process.env.MINIO_ACCESS_KEY,
 	secretKey: process.env.MINIO_SECRET_KEY,
 })
+
+for (const bucket of Object.values(MinioBucket)) {
+	const exists = await MinioClient.bucketExists(bucket)
+
+	if (exists) continue
+
+	Logger.info(`Minio bucket "${bucket}" does not exist.`)
+
+	await MinioClient.makeBucket(bucket, 'us-east-1')
+
+	Logger.info(`Minio bucket "${bucket}" created.`)
+}
 
 export default MinioClient
